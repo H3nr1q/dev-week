2c76057aebd7fee7bbfe66cb2587ff3403180fb8
Casa <carlos_henrique.taqua@hotmail.com>
2022-09-05 23:22:09 -0300

    Terminado CRUD da API usando InMemory

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseInMemoryDatabase("dbContracts"));
+builder.Services.AddScoped<DataBaseContext, DataBaseContext>();
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using src.Models;
+using src.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace src.Controllers;
 
@@ -7,29 +9,42 @@ namespace src.Controllers;
 [Route("[controller]")]
 public class PersonController : ControllerBase
 {
+    private DataBaseContext _context { get; set; }
+    public PersonController(DataBaseContext context)
+    {
+        this._context = context;
+    }   
+
     [HttpGet]
-    public Person GetPerson(){
-        Person person = new Person("Henrique", 34, "12345678");
-        Contract contract = new Contract("abc123", 50.0);
-        person.contracts.Add(contract);
-        person.contracts.Add(contract);
-        person.contracts.Add(contract);
-        return person;        
+    public List<Person> GetListPerson(){
+        // Person person = new Person("Henrique", 34, "12345678");
+        // Contract contract = new Contract("abc123", 50.0);
+        // person.contracts.Add(contract);
+        // person.contracts.Add(contract);
+        // person.contracts.Add(contract);
+        return _context.Persons.Include(p => p.contracts).ToList();
     }
 
     [HttpPost]
-    public Person Post(Person person){
-        
+    public Person Post([FromBody]Person person)
+    {
+        _context.Persons.Add(person);
+        _context.SaveChanges();
         return person;
     }
 
     [HttpPut("{id}")]
     public string UpdatePerson([FromRoute]int id, [FromBody] Person person){
-        return "Dados do id" + id + " atualizados";
+        _context.Persons.Update(person);
+        _context.SaveChanges();
+        return "Dados do id " + id + " atualizados";
     }
 
     [HttpDelete("{id}")]
     public string Delete([FromRoute] int id){
+        var result = _context.Persons.SingleOrDefault(e => e.id == id);
+        _context.Persons.Remove(result);
+        _context.SaveChanges();
         return $"Foi deletado a pessoa do id {id}";
     }
 
